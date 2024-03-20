3dd38b0ca24f61b6c26c2ca19b949fa35b6f2e6c
RyanOrth <ryan.orth@live.com>
2024-03-20 16:26:52 -0500

    Updated build script to WaveFunction, added crabs

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -18,10 +18,7 @@ public class BuildScript
     {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = new[] {
-                "Assets/MirrorExamplesVR/Scenes/SceneVR-Basic/SceneVR-Basic.unity",
-                "Assets/MirrorExamplesVR/Scenes/SceneVR-Common/SceneVR-Common.unity",
-                "Assets/MirrorExamplesVR/Scenes/SceneVR-Menu/SceneVR-Menu.unity",
-                "Assets/MirrorExamplesVR/Scenes/SceneVR-UnityDemo/SceneVR-UnityDemo.unity"
+                "Assets/Scenes/WaveFunction.unity",
             };
         buildPlayerOptions.locationPathName = "Builds/Android/Client/Client.apk";
         buildPlayerOptions.target = BuildTarget.Android;
